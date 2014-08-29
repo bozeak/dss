0bcf01b14ecbe31eecb50a7c7a97cec8fbb1f12c
Bozeak <bozeak@gmail.com>
2014-08-29 20:09:53 +0300

    Trying to secure query

--- worker.php
+++ worker.php
@@ -1,8 +1,24 @@
 <?php
-$email = $_POST['email'];
-$name = $_POST['name'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+//ini_set('display_errors', '1');
+
+$server = "localhost";
+$user = "root";
+$password = "root";
+$db = "feedback";
+
+$mysqli = new mysqli($server, $user, $password, $db);
+
+if ($mysqli->connect_errno) {
+	printf("Connection failed: %s \n", $mysqli->connect_error);
+	exit();
+}
+$mysqli->set_charset("utf8");
+
+
+$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+$subject = mysqli_real_escape_string($mysqli, $_POST['subject']);
+$message = mysqli_real_escape_string($mysqli, $_POST['message']);
 $date = date('r');
 $ip=$_SERVER['REMOTE_ADDR'];
 
@@ -16,24 +32,10 @@ foreach($required as $field) {
 	}
 }
 
-$mysql_server = "localhost";
-$mysql_user = "root";
-$mysql_password = "root";
-$mysql_db = "feedback";
-
-$mysqli = new mysqli($mysql_server, $mysql_user, $mysql_password, $mysql_db);
-
-if ($mysqli->connect_errno) {
-	printf("Connection failed: %s \n", $mysqli->connect_error);
-	exit();
-}
-$mysqli->set_charset("utf8");
-
-
 if($error) {
 	echo "Error"; return false;
 } else {
-	$query = mysqli_query($mysqli, "INSERT INTO messages (email, name, subject, message, ip, date_add) VALUES ('$email', '$name', '$subject', '$message', '$ip', NOW())");
+	$query = mysqli_query($mysqli, "INSERT INTO messages (`email`, `name`, `subject`, `message`, `ip`, `date_add`) VALUES ('$email', '$name', '$subject', '$message', '$ip', NOW())");
 	echo "<script>
 		$('#contact-us').hide();
 		$('.info').html('Thank you for feedback!').addClass(' success');
